fdaf6004404a04360b9a802162b2eaecee78ac3c
Assadmus47 <assadkac47@gmail.com>
2026-06-16 19:41:31 +0200

    feat: add zone models with pydantic and start_hub parser

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,16 @@
 
+from pydantic import BaseModel,Field
+from typing import Optional, List, Literal
+
+
+class Start_hub(BaseModel):
+    name: str = Field(...)
+    x: int = Field(...)
+    y: int = Field(...)
+    color: Optional[str] = None
+    max_drones: int = Field(default=1, gt=0)
+    zone_type: Literal["normal", "blocked", "restricted", "priority"] = "normal"
+
 
 def read_file(filename: str) -> list[str]:
     try:
@@ -7,6 +19,33 @@ def read_file(filename: str) -> list[str]:
     except FileNotFoundError:
         print("ERROR: File Not Found.")
         return []
+
+
+def extract_start_hub(data: list[str]) -> Start_hub:
+    try:
+        metadata = {}
+        data = data.split()
+
+        if len(data) == 4:
+            meta_parts = data[3].strip("[]").split()
+            for elem in meta_parts:
+                elem = elem.split("=")
+                if elem[0] == "color":
+                    metadata["color"] = elem[1]
+                elif elem[0] == "max_drones":
+                    metadata["max_drones"] = int(elem[1])
+                elif elem[0] == "zone":
+                    metadata["zone_type"] = elem[1]
+        zone = Start_hub(
+                name=data[0],
+                x=int(data[1]),
+                y=int(data[2]),
+                **metadata
+            )
+        print(zone)
+
+    except ValueError:
+        raise SystemExit("ERROR : Expected Integer Value")
 
 
 def clean_lines(lines: list[str]) -> list[str]:
@@ -34,6 +73,18 @@ def parse_file(filename: str) -> None:
             f"ERROR line 1: 'nb_drones' value must be a positive integer,"
             f" got '{temp[1].strip()}'"
         )
+    
+    for elem in data[1:]:
+        elem = elem.split(":")
+        if elem[0] == "start_hub":
+            extract_start_hub(elem[1])
+        elif elem[0] == "hub":
+            pass
+        elif elem[0] == "end_hub":
+            pass
+        elif elem[0] == "connection":
+            pass
+
 
 if __name__ == "__main__":
     parse_file("souwa")
